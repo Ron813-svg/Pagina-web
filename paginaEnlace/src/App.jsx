97d4accf10a5d97464d181cdd84c9ebf01bb3184
Rony <ronyjavierramirezalvarado@gmail.com>
2025-03-25 15:37:38 -0600

    v0.2

---
+++ paginaEnlace/src/App.jsx
@@ -0,0 +1,7 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router'
+
+import Calculadora from './Pages/Calculadora.jsx'
+import Card from './Pages/Card.jsx'
+import Componentes from './Pages/Componentes.jsx'
+import Formulario from './Pages/Formulario.jsx'
+import Todo from './Pages/Todo.jsx'
--- paginaEnlace/src/main.jsx
+++ paginaEnlace/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-
+import from
 
 createRoot(document.getElementById('root')).render(
   
